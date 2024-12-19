4cd2947130269f6aa8922dd60f45fac0341c9654
Josh <aftershock840@gmail.com>
2024-12-19 17:44:13 -0500

    updated all functions and variables

--- js/assignment.js
+++ js/assignment.js
@@ -9,27 +9,36 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
 
+const resolution = "To break into a tech job with the help of this codeKY class!";
+const currentYear = 2024;
+
+let willMeetResolution = "yes";
+
 function updateYear() {
     // Update this function
-
+    yearElement.innerText = currentYear;
 }
 
 function updateResolution() {
     // Update this function 
+    resolutionElement.innerText = resolution;
 
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+    willMeetResolutionElement.innerText = willMeetResolution;
 }
 
 function render() {
     // Update this function
+    updateYear();
+    updateResolution();
+    updateWillMeetResolution();
 
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+    render();
 })
