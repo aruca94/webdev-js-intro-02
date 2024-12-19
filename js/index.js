0ae20a915e4a79963d5dbe0cfd0a2dc570fe9c35
Josh <aftershock840@gmail.com>
2024-12-19 17:31:02 -0500

    updated index.js file

--- js/index.js
+++ js/index.js
@@ -2,3 +2,17 @@
 // that are not declared
 "use strict"
 
+
+// const userName = "josh";
+// console.log(userName);
+
+// const num = 115;
+// console.log(typeof num);
+
+// let mentor = true;
+// console.log(typeof mentor);
+
+// mentor = true;
+
+// console.log(mentor);
+// console.log(typeof mentor);
